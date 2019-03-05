9353d3694073a399539b96840f5c7f35c9a2823e
Antonio Ortega <antonioortega130685@gmail.com>
2019-03-05 16:41:25 -0300

    5th commit

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -10,7 +10,18 @@ export class ChartsComponent implements OnInit {
 
   data = {
     datasets: [{
-      data: [10, 20, 30]
+      data: [10, 20, 30],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
     }],
 
     // These labels appear in the legend and in the tooltips when hovering different arcs
@@ -81,28 +92,28 @@ export class ChartsComponent implements OnInit {
       data: {
         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         datasets: [
-            {
-                label: "Prime and Fibonacci",
-                fill: false,
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-            }
+          {
+            label: 'Prime and Fibonacci',
+            fill: false,
+            backgroundColor: 'rgba(54, 162, 235, 1)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+          },
+          {
+            label: 'My Second dataset',
+            fill: true,
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 0.2)',
+            // fillColor: 'rgba(153, 102, 255, 0.2)',
+            // strokeColor: 'rgba(153, 102, 255, 0.2)',
+            // pointColor: 'rgba(153, 102, 255, 0.2)',
+            // pointStrokeColor: "#fff",
+            // pointHighlightFill: "#fff",
+            // pointHighlightStroke: 'rgba(153, 102, 255, 0.2)',
+            data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+          }
         ]
-    },
+      },
       options: {
         scales: {
           yAxes: [{
@@ -111,7 +122,5 @@ export class ChartsComponent implements OnInit {
         }
       }
     });
-
   }
-
 }
